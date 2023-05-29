71226bdb6dbd93493ff9fbd5f2e38f56ea021f7e
nc3 <nc3@kpmg.com.au>
2023-05-29 11:50:31 +0530

    making changes to docker files

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # start-server.sh
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd rfp_app; python manage.py createsuperuser --no-input)
+    (cd martor_demo; python manage.py createsuperuser --no-input)
 fi
-(cd rfp_app; gunicorn KPM.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
+(cd martor_demo; gunicorn KPM.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
 nginx -g "daemon off;"
